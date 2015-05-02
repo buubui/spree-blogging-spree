79b2be4697f7751385d1fe5548151467fe56b4b3
Buu Bui <bhlbuu@gmail.com>
2015-05-02 14:57:03 +0700

    [f] Add meta var for Facebook metadata

--- app/controllers/spree/blog_entries_controller.rb
+++ app/controllers/spree/blog_entries_controller.rb
@@ -1,13 +1,13 @@
 class Spree::BlogEntriesController < Spree::StoreController
-  helper 'spree/blog_entries' 
+  helper 'spree/blog_entries'
 
   before_filter :init_pagination, :only => [:index, :tag, :archive, :author, :category]
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
-  
+
   def index
     @blog_entries = Spree::BlogEntry.visible.page(@pagination_page).per(@pagination_per_page)
   end
-  
+
   def show
     if try_spree_current_user.try(:has_spree_role?, "admin")
       @blog_entry = Spree::BlogEntry.find_by_permalink!(params[:slug])
@@ -15,6 +15,8 @@ class Spree::BlogEntriesController < Spree::StoreController
       @blog_entry = Spree::BlogEntry.visible.find_by_permalink!(params[:slug])
     end
     @title = @blog_entry.title
+    @meta = {}
+    @meta[:keywords] = @blog_entry.summary
   end
 
   def tag
@@ -30,7 +32,7 @@ class Spree::BlogEntriesController < Spree::StoreController
   def archive
     @blog_entries = Spree::BlogEntry.visible.by_date(params).page(@pagination_page).per(@pagination_per_page)
   end
-  
+
   def feed
     @blog_entries = Spree::BlogEntry.visible.limit(20)
     render :layout => false
